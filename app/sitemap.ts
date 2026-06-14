3a01bd0b189ce9fe970f88373bd9ebf5c119c093
Prince Owusu <powusu050@gmail.com>
2026-06-14 16:28:54 +0000

    sitemap update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,15 +1,22 @@
 import type { MetadataRoute } from 'next';
 import { getSiteUrl } from '@/lib/seo';
 
-const MOVIE_CATEGORIES = ['popular', 'airing-today', 'on-tv', 'top-rated'];
+const MOVIE_CATEGORIES = ['popular', 'now-playing', 'upcoming', 'top-rated'];
 const TV_CATEGORIES = ['popular', 'airing-today', 'on-tv', 'top-rated'];
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const siteUrl = getSiteUrl();
+  const siteUrl = getSiteUrl().replace(/\/$/, '');
   const now = new Date();
+
   const staticRoutes = ['/', '/people', '/events', '/search', '/movie', '/tv'];
-  const movieRoutes = MOVIE_CATEGORIES.map((category) => `/movies/${category}`);
-  const tvRoutes = TV_CATEGORIES.map((category) => `/tv/${category}`);
+
+  const movieRoutes = MOVIE_CATEGORIES.map(
+    (category) => `/movies/${category}`
+  );
+
+  const tvRoutes = TV_CATEGORIES.map(
+    (category) => `/tv/${category}`
+  );
 
   return [...staticRoutes, ...movieRoutes, ...tvRoutes].map((route) => ({
     url: `${siteUrl}${route}`,
